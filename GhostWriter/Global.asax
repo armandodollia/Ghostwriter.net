ba06ac9be5ad8d57a1d415f48a47fafe82766729
Armando Dollia <Armando.Dollia@spr.com>
2017-05-30 15:28:52 -0500

    Add project files.

---
+++ GhostWriter/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="GhostWriter.MvcApplication" Language="C#" %>
---
+++ GhostWriter/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GhostWriter.Startup))]
+namespace GhostWriter
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
