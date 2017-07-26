8d804fd9d3ac2302fcdc8fd1821a8a99c41bdd41
armandodollia <dollia.armando@gmail.com>
2017-07-26 09:50:12 -0500

    Moves App_Start folder to correct location

---
+++ GhostWriter/App_Start/IoCConfig.cs
@@ -0,0 +1,32 @@
+﻿using Autofac;
+using Autofac.Extras.CommonServiceLocator;
+using Autofac.Integration.Mvc;
+using Ghostwriter.Entities;
+using Ghostwriter.Repository;
+using Microsoft.Practices.ServiceLocation;
+using System.Reflection;
+using System.Web.Mvc;
+
+namespace GhostWriter
+{
+    public class IoCConfig
+    {
+        public static void RegisterAutoFac()
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterControllers(Assembly.GetExecutingAssembly());
+            builder.RegisterSource(new ViewRegistrationSource());
+            builder.RegisterFilterProvider();
+
+            builder.RegisterType<PostRepository>().As<IPostRepository>().InstancePerRequest();
+            builder.RegisterType<UserRepository>().As<IUserRepository>().InstancePerRequest();
+            builder.RegisterType<CommentRepository>().As<ICommentRepository>().InstancePerRequest();
+            builder.RegisterType<VoteRepository>().As<IVoteRepository>().InstancePerRequest();
+            builder.RegisterType<GhostWriterDbContext>().InstancePerLifetimeScope();
+
+            var container = builder.Build();
+            DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
+        }
+
+    }
+}
---
+++ GhostWriter/App_Start/MappingConfig.cs
@@ -0,0 +1,41 @@
+﻿using Ghostwriter.Entities;
+using Ghostwriter.Entities.Models;
+using GhostWriter.Converters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GhostWriter
+{
+    public static class MappingConfig
+    {
+        public static void RegisterMaps()
+        {
+            AutoMapper.Mapper.Initialize(config =>
+            {
+                config.CreateMap<Post, PostEditViewModel>()
+                .ForMember(dest => dest.Body,
+                           opt => opt.MapFrom(src => src.PostBody));
+
+                config.CreateMap<Post, PostViewModel>()
+                .ForMember(dest => dest.Body,
+                           opt => opt.MapFrom(src => src.PostBody));
+
+                config.CreateMap<Post, PostDetailViewModel>()
+                .ForMember(dest => dest.Body,
+                           opt => opt.MapFrom(src => src.PostBody));
+
+                config.CreateMap<Comment, CommentViewModel>()
+                .ForMember(dest => dest.Body,
+                           opt => opt.MapFrom(src => src.CommentBody));
+
+                config.CreateMap<PostEditViewModel, Post>()
+                .ConvertUsing<PostEditToPostConverter>();
+
+                config.CreateMap<CommentViewModel, Comment>()
+                .ConvertUsing<CommentViewToCommentConverter>();
+            });
+        }
+    }
+}
