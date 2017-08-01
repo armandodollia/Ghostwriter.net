0ae95e46c1aab86181046cd05e784abafa2e23d7
armandodollia <dollia.armando@gmail.com>
2017-08-01 13:13:01 -0500

    Refactors publishing javascript code

--- GhostWriter/Scripts/PostPublishing.js
+++ GhostWriter/Scripts/PostPublishing.js
@@ -1,20 +1,44 @@
 ﻿$(function () {
-    var postId = $("#PostId").val();
-    var publishButton = $("#publishButton");
-
-    publishButton.on("click", function (e) {
-        e.preventDefault;
-        $.ajax({
-            type: "POST",
-            url: "/Posts/Publish/" + postId
-        })
-            .done(function (response) {
-                if (response.IsPublished) {
-                    publishButton.text("Unpublish");
-                }
-                else {
-                    publishButton.text("Publish");
-                }
-            });
-    });
+    var publish = new Publisher(_url);
+    publish.init("#publishButton");
 });
+
+
+function Publisher(url)
+{
+    var _self = this;
+    var _url = url;
+    var _publishButton = null;
+
+    function _onSuccess (data) {
+        if (data) {
+            _publishButton.text(data.IsPublished ? "Unpublish" : "Publish");
+        }
+        else {
+            alert("Could not publish this post");
+        }
+    }
+
+    function _onError (data) {
+        alert("Could not publish this post");
+    }
+
+
+    this._onPublish = function (e) {
+        e.preventDefault;
+        var _postId = $("#PostId").val();
+
+        $.ajax(
+            {
+                type: "POST",
+                url: _url + "/" + _postId,
+                success: function (data) { _onSuccess(data); },
+                error: function (data) { _onError(data); }
+            });
+    };
+
+    this.init = function (publishButtonId) {
+        _publishButton = $(publishButtonId);
+        _publishButton.on("click", this._onPublish);
+    };
+}
