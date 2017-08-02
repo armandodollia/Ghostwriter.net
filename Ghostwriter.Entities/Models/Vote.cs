b04373bb7bdfceb55c1eb08a6acc2a0d95854e91
armandodollia <dollia.armando@gmail.com>
2017-08-02 16:15:44 -0500

    mplements Initial Uniqueness restriction to votes entity

--- Ghostwriter.Entities/Models/Vote.cs
+++ Ghostwriter.Entities/Models/Vote.cs
@@ -7,15 +7,22 @@ namespace Ghostwriter.Entities
     public class Vote : BaseEntity
     {
         [Key]
+        [Column(Order = 1)]
         public int Id { get; set; }
 
         [Required]
         public string VoterId { get; set; }
 
         [ForeignKey("VoterId")]
+        [Column(Order = 2)]
         public virtual ApplicationUser ApplicationUser { get; set; }
 
+        [Key]
+        [Column(Order = 3)]
         public virtual Comment Comment { get; set; }
+
+        [Key]
+        [Column(Order = 4)]
         public virtual Post Post { get; set; }
     }
 }
