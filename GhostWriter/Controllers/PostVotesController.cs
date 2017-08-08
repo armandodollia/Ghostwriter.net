47d92aecd2ea6cca4f5b9da3113a2cf035afb15a
armandodollia <dollia.armando@gmail.com>
2017-08-08 10:43:22 -0500

    Implements Liking Posts via Http Post

--- GhostWriter/Controllers/PostVotesController.cs
+++ GhostWriter/Controllers/PostVotesController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Ghostwriter.Repository;
+using Ghostwriter.Entities.Models;
 
 namespace GhostWriter.Controllers
 {
@@ -16,75 +17,33 @@ namespace GhostWriter.Controllers
             _voteRepository = voteRepository;
         }
 
-        // GET: Votes
-        public ActionResult Index()
-        {
-            return View();
-        }
-
-        // GET: Votes/Details/5
-        public ActionResult Details(int id)
-        {
-            return View();
-        }
-
-        // GET: Votes/Create
-        public ActionResult Create()
-        {
-            return View();
-        }
-
         // POST: Votes/Create
         [HttpPost]
         public ActionResult Create(FormCollection collection)
         {
             try
             {
-                // TODO: Add insert logic here
-
-                return RedirectToAction("Index");
+                var postId = Convert.ToInt32(collection["PostId"]);
+                var vote = new PostVote();
+                vote.PostId = postId;
+                vote.VoterId = GetUserId;
+                _voteRepository.CreateVote(vote);
+                _voteRepository.Save();
+                return RedirectToAction("Details", "Posts", new { Id = postId });
             }
             catch
             {
                 return View();
             }
-        }
-
-        // GET: Votes/Edit/5
-        [Authorize]
-        public ActionResult Edit(int id)
-        {
-            return View();
-        }
-
-        // POST: Votes/Edit/5
-        [HttpPost]
-        public ActionResult Edit(int id, FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add update logic here
-
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: Votes/Delete/5
-        public ActionResult Delete(int id)
-        {
-            return View();
         }
 
         // POST: Votes/Delete/5
         [HttpPost]
-        public ActionResult Delete(int id, FormCollection collection)
+        public ActionResult Delete(FormCollection collection)
         {
             try
             {
+                var test = collection;
                 // TODO: Add delete logic here
 
                 return RedirectToAction("Index");
